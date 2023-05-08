cded3dbd06e0bafdbdf42c27cfe4c583834d608b
tnixc <lauenochlau@gmail.com>
2023-05-08 16:27:07 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <section>
+      <a href="#">wasd</a>
+      <h1>hello</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/nav.vue';
+export default {
+  name: 'App',
+  components: {
+    navbar
+  }
+}
+</script>
+
+<style lang="scss">
+@import 'styles/reset.css'; // css reset
+@import 'styles/variables.scss'; // Catppuccin color scheme
+section {
+  background: DarkSlateBlue;
+  a {
+    color: white;
+  }
+  h1 {
+    color: $text;
+  }
+}
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="navbar">
+    <img src="@/assets/logo.png" alt="">
+    <div class="header-container">
+      <h1>LilyOS</h1>
+      <p>Lorem, ipsum.</p>
+    </div>
+    <div class="seperator"></div>
+    <div class="nav-container">
+      <ul>
+        <li><a href="#">Features</a></li>
+        <li><a href="#">Documentation</a></li>
+        <li><a href="#">Contribute</a></li>
+        <li><a href="#">Download LilyOS</a></li>
+      </ul>
+        <div class="nav-end">
+          <p>Follow</p>
+          <p>placeholder</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+};
+</script>
+<style>
+#navbar{
+  font-family: 'space mono';
+  color: var(--text);
+}
+
+</style>
